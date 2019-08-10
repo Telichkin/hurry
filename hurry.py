c53b68b97e3f9d0eed931d264afc976482d06c03
Roman Telichkin <roman@telichk.in>
2019-08-11 00:36:49 +0300

    Eliminate dependencies from the project

--- hurry.py
+++ hurry.py
@@ -1,9 +1,77 @@
 import os
+import sys
 import json
 import subprocess
 
-from docopt import docopt
+def generate_exec_str(conf):
+    ''' Generate executable string based on config and sys.argv
 
+    Arguments of `hurry` consist of static and dynamic parts which are separated by "--" symbol:
+        
+      $ hurry up service -- --timeout=10000
+             └─[static]─┘  └───[dynamic]───┘
+
+    When static part is the same as a command from the config, function returns appropriate 
+    executable string. Dynamic part is appended to the end of the executable string as is:
+
+      $ cat ./hurry.json   
+      $ { "up service": "docker-compose -f ./composes/service up -d" }
+          └─[command]─┘  └────────────[executable string]───────────┘
+      
+      $ hurry up service -- --timeout=10000
+      $ Execute: docker-compose -f ./composes/service up -d --timeout=10000
+                 └───────────[executable string]──────────┘ └──[dynamic]──┘
+    
+    Command can contain variables, which should be inserted into appropriate place
+    inside executable string:
+
+      $ cat ./hurry.json
+      $ { "up <sname>": "docker-compose -f ./composes/<sname> up -d" }
+             └─[var]─┘                               └─[var]─┘
+      
+      $ hurry up auth ─────────────────────────┐
+                                               V
+      $ Execute: docker-compose -f ./composes/auth up -d
+
+    If function can't find suitable command for givent static part, it returns `None`
+    '''
+    # Divide arguments into static and dynamic parts
+    static, dynamic = [], []
+    for arg in sys.argv[1:]:
+        if (len(dynamic) > 0):
+            dynamic.append(arg)
+        elif (arg == '--'):
+            # Empty string as the first item of dynamic part helps us 
+            # decide in what part of argv we are right now. When dynamic
+            # part is empty we are still in the static part of sys.argv,
+            # otherwise we are in the dynamic part
+            dynamic.append('')
+        else:
+            static.append(arg)
+
+    # Find suitable command for given static part
+    for command, exec_str in conf.items():
+        command = command.split()
+
+        if len(command) != len(static):
+            continue
+                                                         # +---------+--------+------------------------+
+        is_suitable, variables = True, {}                # | command | static |         action         |
+        for cm, st in zip(command, static):              # +---------+--------+------------------------+
+            if cm.startswith('<') and cm.endswith('>'):  # |   up    |   up   |     continue loop      |         
+                variables[cm] = st                       # +---------+--------+------------------------+           
+            elif cm != st:                               # | <name>  | logger |     save veriable      |
+                is_suitable = False                      # |         |        | { "<name>": "logger" } |
+                break                                    # +---------+--------+------------------------+
+                                                         # |  quite  |  loud  |  not suitable, break   |
+        # Replace variable in executable string          # +---------+--------+------------------------+
+        # and add dynamic part if needed
+        if is_suitable:
+            for name, value in variables.items():
+                exec_str = exec_str.replace(name, value)
+            if len(dynamic) > 1:
+                exec_str += ' '.join(dynamic)
+            return exec_str
 
 def main():
     # Read json config from the "hurry.json" file in the current dir
@@ -15,26 +83,12 @@ def main():
         print("Can't find hurry.json in the current folder")
         return
 
-    # Transform read json into valid docopt string
-    docopt_str = 'Usage:'
-    for cmd in conf:
-        docopt_str += ('\n    hurry ' + cmd)
-    
-    # Transform current command-line arguments into docopt format
-    args = docopt(docopt_str, options_first=True)
-
-    exec_str = compose_exec_str(conf, args)
-    print("Execute: " + exec_str)
-    subprocess.call(exec_str, shell=True)
-
-
-def compose_exec_str(conf, parsed_args):
-    enabled_args = {k: v for k, v in parsed_args.items() if v}
-
-    for k in conf:
-        if set(k.split()) == set(enabled_args.keys()):
-            exec_str = conf[k]
-            for arg, v in enabled_args.items():
-                if not isinstance(v, bool):
-                    exec_str = exec_str.replace(arg, v)
-            return exec_str
+    exec_str = generate_exec_str(conf)
+    if exec_str:
+        print("Execute: " + exec_str)
+        subprocess.call(exec_str, shell=True)
+    else:
+        help_str = 'Usage:'
+        for cmd_template in conf:
+            help_str += ('\n    hurry ' + cmd_template)
+        print(help_str)
--- tests/test_hurry.py
+++ tests/test_hurry.py
@@ -7,21 +7,53 @@ import pytest
 
 import hurry
 
+def test_generate_exec_str():
+    cmd = {
+        'one': 'one command',
+        'two words': 'two words command',
+        'start <name>': 'docker run <name>',
+        'end <arg1> <arg2>': 'arg1=<arg1> arg2=<arg2>',
+    }
+
+    sys.argv = ['hurry', 'one']
+    assert hurry.generate_exec_str(cmd) == 'one command'
+
+    sys.argv = ['hurry', 'two', 'words']
+    assert hurry.generate_exec_str(cmd) == 'two words command'
+
+    sys.argv = ['hurry', 'start', 'service']
+    assert hurry.generate_exec_str(cmd) == 'docker run service'
+
+    sys.argv = ['hurry', 'end', 'test', 'prod']
+    assert hurry.generate_exec_str(cmd) == 'arg1=test arg2=prod'
+
+    sys.argv = ['hurry', 'one', 'unknown']
+    assert hurry.generate_exec_str(cmd, ) is None
+
 def test_run_hurry_e2e(create_hurry_json, tmpdir):
     create_hurry_json({
-        "write <file_path>": "echo 'this is e2e test' > <file_path>",
-        "read <file_path>": "cat <file_path>",
+        'write <file_path>': 'echo "this is e2e test" > <file_path>',
+        'read <file_path>': 'cat <file_path>',
     })
     test_file = tmpdir.join("test.txt")
 
-    stdout = execute("hurry write " + str(test_file))
+    stdout = execute('hurry write ' + str(test_file))
 
-    assert stdout.decode().strip() == "Execute: echo 'this is e2e test' > " + str(test_file)
-    assert test_file.read().strip() == "this is e2e test"
+    assert stdout.decode().strip() == 'Execute: echo "this is e2e test" > ' + str(test_file)
+    assert test_file.read().strip() == 'this is e2e test'
 
     stdout = execute('hurry read some/path')
 
     assert stdout.decode().strip() == 'Execute: cat some/path'
+
+    stdout = execute('hurry unknown')
+
+    help_msg = 'Usage:\n    hurry write <file_path>\n    hurry read <file_path>'
+    assert stdout.decode().strip() == help_msg
+
+    stdout = execute('hurry read file -- hello')
+
+    assert stdout.decode().strip() == 'Execute: cat file hello'
 
 
 def execute(command):
